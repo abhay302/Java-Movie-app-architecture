96b0b31892561aee2952b1bde211eeabbf523e8f
abhay302 <39900609+abhay302@users.noreply.github.com>
2018-06-16 17:59:19 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -6,41 +6,41 @@ import java.io.*;
  */
 class Application {
 	
-	public static void main(String... args) throws IOException {
-		MovieManager manager=new MovieManager();
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			System.out.println("1. Get all movies");
-			System.out.println("2. Get movie of a specific type");
-			System.out.println("3. Check whether a movie is blockbuster");
-			System.out.println("4. Exit");
-			System.out.println("Enter choice");
-			int n=Integer.parseInt(br.readLine());
-			if(n==4)
-				break;
+    public static void main(String... args) throws IOException {
+        MovieManager manager=new MovieManager();
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            System.out.println("1. Get all movies");
+            System.out.println("2. Get movie of a specific type");
+            System.out.println("3. Check whether a movie is blockbuster");
+            System.out.println("4. Exit");
+            System.out.println("Enter choice");
+            int n=Integer.parseInt(br.readLine());
+            if(n==4)
+                break;
 			
-			switch(n) {
-			case 1:
-				manager.showAllMovies();
-				break;
-			case 2:
-				System.out.println("Enter type \n 1. Tollywood \n 2. Bollywood \n 3. Hollywood");
-				int type=Integer.parseInt(br.readLine());
-				if(type>=0&&type<=2)
-					manager.showSpecificMovie(type);
-				else
-					System.out.println("Invalid type");
-				break;
-			case 3:
-				System.out.println("Enter movie name");
-				String name=br.readLine();
-				manager.isBlockBuster(name);
-				break;
-			default:
-				System.out.println("Invalid choice");
+            switch(n) {
+            case 1:
+                manager.showAllMovies();
+                break;
+            case 2:
+                System.out.println("Enter type \n 0. Tollywood \n 1. Bollywood \n 2. Hollywood");
+                int type=Integer.parseInt(br.readLine());
+                if(type>=0&&type<=2)
+                    manager.showSpecificMovie(type);
+                else
+                    System.out.println("Invalid type");
+                break;
+            case 3:
+                System.out.println("Enter movie name");
+                String name=br.readLine();
+                manager.isBlockBuster(name);
+                break;
+            default:
+                System.out.println("Invalid choice");
 				
-			}
+            }
 			
-		}
-	}
+        }
+    }
 }
