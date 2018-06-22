c424fc67fc0ce3a87e6c3e9295759b678a6ff9fe
abhay302 <39900609+abhay302@users.noreply.github.com>
2018-06-22 15:30:11 +0530

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -8,10 +8,11 @@ enum Type {
     TOLLYWOOD, BOLLYWOOD, HOLLYWOOD;
 }
 
-/* Movie is the most important Entity of the application.
- * A movie will have name, year, language, genre and release date associated with it.
- * A movie can be either TollyWood, BollyWood or HollyWood.
- * The average cost of production will depend on the type of the movie.
+/**
+ * Movie is the most important Entity of the application. A movie will have
+ * name, year, language, genre and release date associated with it. A movie can
+ * be either TollyWood, BollyWood or HollyWood. The average cost of production
+ * will depend on the type of the movie.
  */
 class Movie {
 
@@ -21,31 +22,36 @@ class Movie {
     private String genre;
     private Date releaseDate;
     private Type type;
+    
+    // Specify whether the movie is block buster or not
     private boolean blockBuster;
-    private boolean accessed; // Specify whether the movie has been already checked for being a block buster or not
-    private int avgCostOfProduction;
+
+    /*
+     * Specify whether the movie has been already checked for being a block buster
+     * or not. Initially assigned as false indicating that this movie is not yet
+     * checked for being a block buster
+     */
+    private boolean accessed = false;
 
     // Constructor to initialize the movie object
-    public Movie(String name, int year, String language, String genre, Date releaseDate, Type type, int avgCostOfProduction) {
+    public Movie(String name, int year, String language, String genre, Date releaseDate, Type type) {
         this.name = name;
         this.year = year;
         this.language = language;
         this.genre = genre;
         this.releaseDate = releaseDate;
         this.type = type;
-        this.avgCostOfProduction = avgCostOfProduction;
     }
-    
+
     // Override toString() method to display the content of the movie object
     @Override
     public String toString() {
-        String s="";
-        s+= "\nName:\t\t\t\t" + name + "\n";
-        s+= "Year:\t\t\t\t" + year + "\n";
-        s+= "Language:\t\t\t" + language + "\n";
-        s+= "Genre:\t\t\t\t" + genre + "\n";
-        s+= "Release Date:\t\t\t" + DateFormat.getDateInstance(DateFormat.MEDIUM).format(releaseDate) + "\n";
-        s+= "Average Cost of production:\t" + avgCostOfProduction + "\n";
+        String s = "";
+        s += "\nName:\t\t\t\t" + name + "\n";
+        s += "Year:\t\t\t\t" + year + "\n";
+        s += "Language:\t\t\t" + language + "\n";
+        s += "Genre:\t\t\t\t" + genre + "\n";
+        s += "Release Date:\t\t\t" + DateFormat.getDateInstance(DateFormat.MEDIUM).format(releaseDate) + "\n";
         return s;
     }
 
@@ -63,5 +69,10 @@ class Movie {
 
     public void setAccessed(boolean accessed) {
         this.accessed = accessed;
-    }    
+    }
+
+    public Type getType() {
+        return type;
+    }
+
 }
